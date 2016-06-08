5acc9cb26c6bbba647155c330b5d1c0cd7ed5290
Oakland Peters <oakland.peters@gmail.com>
2016-06-08 18:03:58 -0400

    Grid/Point mapping functions

--- src/grid.ts
+++ src/grid.ts
@@ -1,22 +1,22 @@
 /**
- * Point class hierarchy is unnecessarily complex,
- * as an exercise in learning TypeScript/ES6 classes
+ * Grid and Point class hierarchy.
+ * The ability to lookup based on coordinates is two way between Point<-->Grid.
+ * 
  */
 
-import {PointInterface, GridInterface, KindInterface, Buildable} from './interfaces';
-import {Kind, AllKinds, CumulativeDistribution} from './agents';
+import {PointInterface, GridInterface, KindInterface, OrderedActionsInterface, ActionInterface, CoordinatesInterface, Buildable} from './interfaces';
+import {Kind, AllKinds} from './agents';
+import {CumulativeDistribution} from './distribution';
 
 
 
-// export class PointBase implements PointInterface {
-// export class PointBase implements ConstructibePointInterface {
-export class PointBase implements ConstructiblePoint {
+export class PointBase implements PointInterface {
 	/**
 	 * Base class shared by all point implementations.
 	 * Concrete point classes vary by their dimensionality.
 	 */
 	constructor(
-		public coordinates: Array<number>,
+		public coordinates: CoordinatesInterface,
 		public kind: KindInterface = AllKinds.empty()
 	) { };
 
@@ -41,7 +41,7 @@ export class PointBase implements ConstructiblePoint {
 		return "[${inner}]";
 	}
 
-	map(func: (value: number, index?: number, thisValues?: Array<number>) => number): this {
+	mapCoordinates(func: (value: number, index?: number, thisValues?: CoordinatesInterface) => number): this {
 		/*
 		Map over the points, but not the kind.
 		This is currently immutable, which is inefficient. Either adopt a library for more
@@ -57,13 +57,22 @@ export class PointBase implements ConstructiblePoint {
 		)
 	}
 
+	map(func: (point: this, index?: number) => this): this {
+		/**
+		 * Very basic map function. Exists to provide easier mapping behavior on grids.
+		 * (so you can call map on a row of a grid, or on a point of a grid), without reflecting
+		 * on the type of the object (grid/row/point)
+		 */
+		return func(this);
+	}
+
 	invert(): this {
-		return this.map((x, index, thisValues) => -x);
+		return this.mapCoordinates((x, index, thisValues) => -x);
 	}
 
 	add(right: this): this {
 		return this.fromArray(
-			this.coordinates.map((value: number, index: number, array: number[]) => value + right[index])
+			this.coordinates.map((value: number, index: number, array: number[]) => value + right.coordinates[index])
 		);
 	}
 
@@ -83,14 +92,14 @@ export class PointBase implements ConstructiblePoint {
  * Distinct to prevent mixing them up together, and to allow pairing the class with Grid classes.
  */
 export class Point2D extends PointBase {
-	constructor(coordinates: Array<number> = [0, 0], kind: KindInterface = AllKinds.empty()) {
+	constructor(coordinates: CoordinatesInterface = [0, 0], kind: KindInterface = AllKinds.empty()) {
 		assert(coordinates.length == 2)
 		super(coordinates, kind)
 	}
 }
 
 export class Point3D extends PointBase {
-	constructor(coordinates: Array<number> = [0, 0, 0], kind: KindInterface = AllKinds.empty()) {
+	constructor(coordinates: CoordinatesInterface = [0, 0, 0], kind: KindInterface = AllKinds.empty()) {
 		assert(coordinates.length == 3)
 		super(coordinates, kind)
 	}
@@ -101,73 +110,61 @@ export class Point3D extends PointBase {
 /**
  * Grid Classes
  */
-export abstract class GridBase<PointClass extends PointBase> implements GridInterface {
+export class GridBase<PointClass extends PointBase> implements GridInterface {
 	Point: PointClass;
 	size: number[];
 	points: Array<any | PointClass>;  // Recursive data type. type T = Array<T> | T;
 
-	makePoint(coordinates: number[]): PointClass {
+
+	// Constructors
+	// It's not conventional for abstract classes to have constructors...
+	// So I'll likely either move these into child classes, or make this class no longer be abstract
+	fromArray(points: PointClass[]) {
+		return new (this.constructor as Buildable<this>)(...points);
+	}
+
+	makePoint(coordinates: CoordinatesInterface): PointClass {
 		return this.Point.constructor.apply({}, coordinates);
 	}
-	protected fetch(coordinates: number[]): any {
+	protected fetch(coordinates: CoordinatesInterface): any {
 		assert(this.size.length >= coordinates.length);
 		let lense = this.points;
 		coordinates.forEach((coordinate) => lense = lense[coordinate]);
 		return lense;
 	}
-	getPoint(coordinates: number[]): PointClass {
+	protected setter(coordinates: CoordinatesInterface, point: PointClass): void {
+		/** Path based setter.
+		var grid = new Grid(....)
+		grid.setter([0, 1, 4], new Point3D(...))
+		*/
+		let front = coordinates.slice(0, -1);
+		let last = coordinates.slice(-1)[0];
+		let deepestArray = this.fetch(front);
+		deepestArray[last] = point;
+	}
+	getPoint(coordinates: CoordinatesInterface): PointClass {
 		return this.fetch(coordinates) as any as PointClass;
 	}
-	setPoint(coordinates: number[], kind: Kind): PointClass {
-		this.fetch(coordinates) = kind;
+	setPoint(coordinates: CoordinatesInterface, kind: Kind): void {
+		let point = this.fetch(coordinates);
+		point
 	}
-}
-
-/*
-function Init3DimensionalArray(xmax, ymax, zmax, def) {
-    var r, x, y, z;
-    for (r = [], x = 0; x < xmax; x++)
-        for (r [x] = [], y = 0; y < ymax; y++)
-             for (r [x][y] = [], z = 0; z < zmax; x++)
-                  r [x][y][z] = def;
-    return r;
-}
-var my3DimArray = Init3DimensionalArray(5, 4, 3, 0);
- */
-
-
-function product<T>(...pools: Array<Array<T>>): Array<Array<T>> {
-	/* Cartesian product of arrays.
-	product(['A', 'B', 'C', 'D'], ['x', 'y']) --> [['A', 'x'], ['A', 'y'], ['B', 'x'], ...]
-	*/
-	let accumulator: Array<Array<T>> = [[]];
-	pools.forEach(function(pool) {
-		var temp: Array<Array<T>> = [];
-		accumulator.forEach(function(prior) {
-			pool.forEach(function(entry) {
-				let _t = prior.slice();
-				_t.push(entry)
-				temp.push(_t)
-			});
-		})
-		accumulator = temp;
-	})
-	return accumulator;
-}
-
-
-function range(start: number, stop: number, step: number = 1) {
-	/* Modified from Underscore's range() function. */
-    let length = Math.max(Math.ceil((stop - start) / step), 0);
-    let range = Array(length);
-    for (let idx = 0; idx < length; idx++ , start += step) {
-		range[idx] = start;
-    }
-    return range;
-};
-
-function assert(value: Boolean, message: string = "Invalid assertion.") {
-	if (!value) {
-		throw Error(message)
+	map(pointFunction: (point: PointInterface, index?: CoordinatesInterface, thisContainer?: this) => PointInterface): this {
+		// Proxies the pointFunction call down the array of points.
+		// This should be chained downward until it hits a 'Point' - whose map function will apply it
+		return this.fromArray(
+			this.points.map((thing, index, thisContainer) => thing.map(pointFunction, index, thisContainer))
+		);
+	}
+	initialize(distribution: CumulativeDistribution<KindInterface>): this {
+		this.map((point: PointClass) => distribution.random());
+		return this;
+	}
+	step(actions: OrderedActionsInterface): this {
+		let grid = this;
+		actions.map(function(action){
+			grid = grid.map((point, index) => action.step(point, grid))
+		});
+		return grid;
 	}
 }
