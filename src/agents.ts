48a0c860c1d211498fdb91cce15c97bc350ecf53
Oakland Peters <oakland.peters@gmail.com>
2016-06-08 18:04:27 -0400

    Remove random and Distribution from agents.ts

--- src/agents.ts
+++ src/agents.ts
@@ -24,7 +24,7 @@ export class Kind implements KindInterface {
 	) { }
 
 	register(): void {
-		AllKinds.add(this);
+		AllKinds.push(this);
 	}
 }
 
@@ -33,72 +33,6 @@ export var EmptyKind = new Kind('Empty', 0).register();
 
 
 
-
-
-export class Distribution<T> extends Array<[number, T]> {
-	normalize(): this {
-		let total = this.reduce((_sum: number, pair: [number, T]) => _sum + pair[0], 0);
-		let normalized = this.map(pair => [pair[0]/total, pair[1]]);
-		//
-		return this.constructor.apply({}, normalized);
-		// return Distribution.apply({}, normalized);
-	}
-	integral(): CumulativeDistribution<T> {
-		let cumulative = 0;
-		let accumulator = new CumulativeDistribution<T>();
-		let total = this.reduce((_sum: number, pair: [number, T]) => _sum + pair[0], 0);
-		this.forEach(function(pair: [number, T]) {
-			cumulative = cumulative + pair[0];
-			accumulator.push([cumulative / total, pair[1]]);
-		});
-		return accumulator;
-	}
-}
-
-export class CumulativeDistribution<T> extends Array<[number, T]> {
-	normalize(): this {
-		let total = this.reduce((_sum: number, pair: [number, T]) => _sum + pair[0], 0);
-		let normalized = this.map(pair => [pair[0] / total, pair[1]]);
-		return this.constructor.apply({}, normalized);
-	}
-	random(seed: number = 0 ): T {
-		let rando = random.unit();
-		this.forEach(function(pair) {
-			if (rando <= pair[0]) {
-				return pair[1] as T;
-			}
-		});
-		throw Error("Incorrectly constructred CumulativeDistribution - no entry matched random number: " + String(rando));
-	}
-}
-
-
-// export class AgentDistribution extends Array<KindDistP> {
-// 	integral(): CumulativeAgentDistribution {
-
-// 		let total = this.reduce((_sum: number, pair: KindDistP) => _sum + pair[0], 0);
-
-
-// 		let cumulative = 0;
-// 		let accumulator = new AgentCumulativeDistribution();
-// 		this.forEach(function(pair: KindDistP) {
-// 			cumulative = cumulative + pair[0];
-// 			accumulator.push([cumulative / total, pair[1]);
-// 		});
-// 		return accumulator;
-// 	}
-// }
-
-// export class CumulativeAgentDistribution extends Array<KindDistP> {
-// 	random(seed: number = 0) {
-
-// 	}
-// }
-
-
-/*
-Quality of life utilties, should probably go into a different module
-*/
 
 function* values(obj: any): any {
 	/* Retreive values with a generator expression */
@@ -112,21 +46,3 @@ function construct<T extends Function>(self: T, args: Array<any>): T {
 	return self.apply(self, args);
 }
 
-export class Random {
-	/*
-	Short-term hack. I need to install a better library for this.
-	*/
-	constructor(public seed: number = 0) { }
-	foldSeed(seed: number): number {
-		return (seed * 9301 + 49297) % 233280;
-	}
-	range(seed: number = this.seed, min = 0, max = 1) {
-		this.seed = this.foldSeed(seed);
-		let rando = this.seed / 233280;
-		return min + rando * (max - min);
-	}
-	unit(seed: number = this.seed) {
-		return this.range(seed);
-	}
-}
-export var random: Random = new Random(0);
