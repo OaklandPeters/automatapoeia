e7c401f10ed52aae8ca785e312ceece46f074fef
Oakland Peters <oakland.peters@gmail.com>
2016-07-21 13:47:00 -0400

    Update foldable constructors

--- src/cats/foldable.ts
+++ src/cats/foldable.ts
@@ -45,29 +45,6 @@ function none<T>(foldable: Foldable<T>, predicate: (value: T) => boolean = Boole
 		accumulator || predicate(element), false);
 }
 
-/* Constructors
-===================== */
-var From = {
-
-}
-
-var To = {
-
-}
-
-function foldable_to_array<T>(foldable: Foldable<T>): Array<T> {
-	return foldable.fold<Array<T>>((acc, elm) => acc.concat([elm]), [] as Array<T>)
-}
-
-function foldable_to_iterator<T, U>(foldable: Foldable<T>, initial: U,
-	folder: (accumulator: U, element: T) => U
-	): Iterator<T> {
-	/* This requires either some sort of queue (such as an array), or 
-	pausable exectuion (yield). The array is more-memory intensive, but
-	the 'yield' approaches are not yet widely available in browsers. */
-	return array_to_iterable(foldable_to_array(foldable)).iter()
-}
-
 function foldIterable<T, U>(iterable: Iterable<T>, initial: U,
 	folder: (accumulator: U, element: T) => U): U {
 	let accumulator = initial;
@@ -77,20 +54,71 @@ function foldIterable<T, U>(iterable: Iterable<T>, initial: U,
 	return accumulator;
 }
 
-class IterableToFoldable<T> extends Foldable<T> {
-	iterable: Iterable<T>;
-	iterator: Iterator<T>;
-
-	constructor(iterable: Iterable<T>) {
-		super();
-		this.iterable = iterable;
-	}
-
-	fold<U>(folder: (accumulator: U, element: T) => U, initial: U): U {
-		return foldIterable<T, U>(this.iterable, initial, folder);
+/* Constructors
+===================== */
+var From = {
+	Iterable: function<T>(iterable: Iterable<T>): Foldable<T> {
+		return {
+			fold: function<U>(f: FoldFunc<T, U>, initial: U): U {
+				return foldIterable<T, U>(iterable, initial, f);
+			}
+		}
 	}
 }
 
+var To = {
+	Array: function foldable_to_array<T>(foldable: Foldable<T>): Array<T> {
+		return foldable.fold<Array<T>>((acc, elm) => acc.concat([elm]), [] as Array<T>)
+	},
+	Iterator: function foldable_to_iterator<T, U>(foldable: Foldable<T>, initial: U,
+		folder: (accumulator: U, element: T) => U
+		): Iterator<T> {
+		/* This requires either some sort of queue (such as an array), or 
+		pausable exectuion (yield). The array is more-memory intensive, but
+		the 'yield' approaches are not yet widely available in browsers. */
+		return IterableFrom.Array<T>(To.Array<T>(foldable)).iter()
+	}
+}
+
+/* Functors & Metafunctions
+Functions that modify or decorate morphisms in this category
+============================================================== */
+function filter<T>(foldable: Foldable<T>, predicate: PredicateFunc<T>): Foldable<T> {
+	// Two ways to do this -- either
+	// (1) return a simple foldable structure with only the 'fold' method
+	return  {
+		original: foldable,
+		fold: function<U>(f: FoldFunc<T, U>, initial: U): U {
+			let filter_f = function(accumulator: U, element: T): U {
+				if (predicate(element)) {
+					return f(accumulator, element)
+				} else {
+					return accumulator
+				}
+			}
+			return this.original.fold(filter_f, initial)
+		}
+	} as Foldable<T>
+	// (2) try to clone the object, but with this new fold function
+	// 	This will work well once ES6 'Proxy's are available
+	// let new_foldable = Object.create(foldable);
+	// new_foldable.fold = new_fold
+	// return new_foldable
+}
+
+function filterFoldFunction<T, U>(foldF: FoldFunc<T, U>, predicate: PredicateFunc<T>): FoldFunc<T, U> {
+	/* 
+	Technically speaking, this is "An endomorphism transforming morphisms in
+	the category of Foldable to other morphisms in the same category."
+	*/
+	return function(accumulator: U, element: T): U {
+		if (predicate(element)) {
+			return foldF(accumulator, element)
+		} else {
+			return accumulator
+		}
+	}
+}
 
 
 
