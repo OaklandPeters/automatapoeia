017d393be434c7932a5a86bcb2a610ae93ad8a5e
Oakland Peters <oakland.peters@gmail.com>
2016-07-26 17:29:55 -0400

    Project cruft.

--- src/cats/all_cats.ts
+++ src/cats/all_cats.ts
@@ -1,10 +1,18 @@
 
 import * as iterable from './iterable';
+import * as iterator from './iterator';
+import * as iteration_result from './iteration_result';
+import * as indexable from './indexable';
 import * as foldable from './foldable';
 import * as equatable from './equatable';
 import * as liftable from './liftable';
 import * as zeroable from './zeroable';
 import * as record from './record';
+import * as appendable from './appendable';
+import * as reducible from './reducible';
+import * as sized from './sized';
+import * as typecheckable from './typecheckable';
+
 
 // import * as mappable from './mappable';
 // import * as sequence from './sequence';
