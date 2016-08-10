977be7fce42510cd0b28ad2142f8a16af99c0361
Oakland Peters <oakland.peters@gmail.com>
2016-08-09 22:56:37 -0400

    Unites class and assertions.

--- src/cats/cat_support.ts
+++ src/cats/cat_support.ts
@@ -43,17 +43,131 @@ type NativeKeyedCollections = (Map<any, any> | WeakMap<any, any>
 // Class<T> is used to refer to construction functions for a given class
 type Class<T> = {new(...values: Array<any>): T};
 
+/* Assertions
+===================================== */
+interface IException {
+    name: string;
+    message: string;
+    stack: string|void;
+}
+
+class Exception implements IException {
+	constructor(
+		public name: string,
+		public message?: string,
+		public stack?: string) {
+	}
+}
+
+class AssertionException extends Exception {
+	constructor(message?: string, stack?: string) {
+		let msg = (typeof message === 'undefined') ? "Assertion untrue" : message;
+		super("AssertionException", msg, stack);
+	}
+	static is(value: any): value is AssertionException {
+		if (value.name instanceof String) {
+			if (value.name.toLowerCase().startsWith('assertion')) {
+				return true;
+			}
+		}
+		return false
+	}
+}
+
+function assert(value: boolean | (() => boolean), message?: string): boolean {
+	let truth = (typeof value === 'function') ? value() : value;
+	if (truth === false) {
+		throw new AssertionException(message);
+	} else {
+		return truth;
+	}
+}
+
 function assertType<T>(value: any, predicate: (x: any) => boolean): value is T {
 	/* Use this as an ad-hoc type-gaurd. Especially useful for checking built-in types
 	without defining a huge number of custom functions. Ex.
 		if (assertType<string>(x, (x) => (typeof x === 'string') )) {
 	*/
-	return predicate(value)
+	return assert(() => predicate(value))
+}
+
+/* Law-based unit-testing utility
+===================================== */
+interface TestLog extends Array<[any]> {}
+
+class UnitTests {
+	/*
+	random: A function that generates random instances of class T
+
+	@todo: refactor this into UnitTest, and LawTests which extends it
+	@todo: Give a better type to the TestLog
+	*/
+	log: TestLog;
+	constructor() {
+		this.log = [];
+	}
+	run(names: Array<string>): TestLog {
+		for (name of names) {
+			let method = (this as any)[name];
+			// Execute the test methods - catching assertion errors
+			let passed = true;
+			let exception: Error;
+			try {
+				method();
+			} catch (err) {
+				passed = false;
+				exception = err;
+			}
+			if (passed) {
+				this.recordSuccess(name);
+			} else {
+				this.recordException(name, exception);
+			}
+		}
+		return this.log;
+	}
+	runAll() {
+		let methodNames: Array<string> = [];
+		for (let key in this) {
+			if (key.startsWith('test')) {
+				methodNames.push(key);
+			}
+		}
+		return this.run(methodNames);
+	}
+	recordException(methodName: string, exception: Error | AssertionException) {
+		if (AssertionException.is(exception)) {
+			return this.recordFail(name, exception);
+		} else {
+			return this.recordError(name, exception);
+		}
+	}
+	recordFail(methodName: string, error: AssertionException) {
+		this.log.push(['Fail', methodName, error.name, error.message]);
+	}
+	recordError(methodName: string, error: Error) {
+		this.log.push(['Error', methodName, error.name, error.message]);
+	}
+	recordSuccess(methodName: string) {
+		this.log.push(['Success', methodName]);
+	}
+}
+
+class LawTests<Klass> extends UnitTests {
+	constructor(
+		public klass: {new: (values: Array<any>) => Klass},
+		public random: (seed: number) => Klass,
+		public seed: number = 0
+	) {
+		super();
+	}
 }
 
 
 export {
 	Builtin, Primitive,
 	Native, NativeMisc, NativeIndexedCollections, NativeKeyedCollections,
-	Class, assertType
+	Class, assertType, assert,
+	Exception, AssertionException,
+	LawTests
 }
