c1d079b1fac3c1a4a917681ec753cefa2b144348
Oakland Peters <oakland.peters@gmail.com>
2016-10-05 17:14:14 -0400

    Cleanup category template

--- src/cats/cat_template.ts
+++ src/cats/cat_template.ts
@@ -26,19 +26,19 @@
 with 'is' type-checking static method
 ========================================= */
 
+/* Generic functions
+for each abstract method
+================================================ */
+
+/* Type-Guards: Type-checking functions
+================================================= */
+
 /* Laws
 Assertion functions expressing something which must
 be true about the category for it to be sensible, and
 are part of its definition, but are not expressible
 in the type-system.
 ================================================= */
-
-/* Type-Guards: Type-checking functions
-================================================= */
-
-/* Generic functions
-for each abstract method
-================================================ */
 
 /* Derivable functions
 these are the real stars of the show - the functions
@@ -52,6 +52,11 @@ Functions that modify or decorate morphisms in this category
 /* Constructors
 convert between elements (~instances) of two categories
 ==================================== */
+var From = {
+};
+
+var To = {
+};
 
 /* Functors
 convert between morphisms (~functions) of two categories.
@@ -61,6 +66,8 @@ convert between morphisms (~functions) of two categories.
 equivalents to this categories' method,
 for built-in Javascript types
 ====================================== */
+var Native = {
+};
 
 /* Exports
 ==================== */
