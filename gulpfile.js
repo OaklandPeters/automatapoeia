a2cda58eeb17b46ab073bcc6de3aa106ee6dae65
Oakland Peters <oakland.peters@gmail.com>
2016-11-30 10:24:17 -0500

    Add notes explaining build process

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var browserify = require('gulp-browserify');
 var exec = require('child_process').exec;
 var babel = require('gulp-babel');
 
+// Compile Typescript: src/ --> build/es6/
+//      Typescript compiler
+//      Destination folder specified in tsconfig.json
+//      Hinges on: typings/tsd.d.ts and src/main.ts
 gulp.task('build', function(callback){
     exec('tsc -p .', function(error, stdout, stderr) {
         if(stdout){ console.log(stdout) }
@@ -16,6 +20,8 @@ gulp.task('build', function(callback){
    });
 });
 
+// Compile to ES5: build/es6/ --> build/es5/
+//      Babel compiler - includes browser compatability
 gulp.task('es5', ['build'], function() {
 	return gulp.src('build/es6/**/*.js')
 		.pipe(babel({
@@ -24,10 +30,13 @@ gulp.task('es5', ['build'], function() {
 		.pipe(gulp.dest('build/es5'));
 });
 
+// Bundle code into one JS: build/es5/ --> build/es5/main.js
+//      Browersify for Desktop/Browser compatability
 gulp.task('browserify', ['es5'], function(){
 	return gulp.src('build/es5/main.js')
 		.pipe(browserify())
 		.pipe(gulp.dest('build/merged/'))
 });
 
+// Default task: runs TS --> ES6 --> ES5
 gulp.task('default', ['browserify']);
